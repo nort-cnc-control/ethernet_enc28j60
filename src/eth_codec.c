f346c84e0ad9ade56167969c5fd78f469f803209
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2019-08-01 21:20:05 +0300

    Add codec

---
+++ src/eth_codec.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <eth_codec.h>
+
+uint16_t enc28j60_get_ethertype(const uint8_t *data, size_t len)
+{
+    if (len < 14)
+        return 0xFFFF;
+    return ((uint16_t)data[12]) << 8 | (data[13]);
+}
+
+const uint8_t *enc28j60_get_target(const uint8_t *data, size_t len)
+{
+    return data;
+}
+
+const uint8_t *enc28j60_get_source(const uint8_t *data, size_t len)
+{
+    return data + 6;
+}
+
+const uint8_t *enc28j60_get_payload(const uint8_t *data, size_t len)
+{
+    return data + ETHERNET_HEADER_LEN;
+}
+
+void enc28j60_fill_header(uint8_t *buf, const uint8_t *src, const uint8_t *dst, uint16_t ethertype)
+{
+    memcpy(buf, dst, 6);
+    memcpy(buf+6, src, 6);
+    buf[12] = ethertype >> 8;
+    buf[13] = ethertype & 0xFF;
+}
